2d43393319631aa4017e663d3f2082e04f28db96
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-04-10 10:20:50 -0400

    first commit

---
+++ atividade01.js
@@ -0,0 +1,23 @@
+let nome = `Amanda`;
+let idade = 16;
+
+console.log(`ola, meu nome é`,nome,'e eu tenho',idade,'anos')
+
+//2
+let string = `dois patinhos`
+palavras = string.toLowerCase();
+console.log(palavras);
+
+palavras = string.toLocaleUpperCase();
+console.log(palavras);
+
+//3
+let numero = 80;
+console.log(typeof numero)
+
+//4
+let altura = 1.70
+let peso = 23
+let soma = (peso / (altura*altura)).toFixed(2);
+console.log(soma);
+
