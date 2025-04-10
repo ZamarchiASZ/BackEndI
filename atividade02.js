04f5bca00cb9987185dfdef24a96547a9f9cdf51
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-04-10 11:21:14 -0400

    atividade aula 2

---
+++ atividade02.js
@@ -0,0 +1,25 @@
+//01
+let num1 = 14;
+let num2 = 45;
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+
+//02
+let idade1 = 16;
+let idade2 = 36;
+
+let verificar = idade1 > idade2 ? "A primeira pessoa é a mais velha!": "A segunda pessoa é a mais velha!"
+console.log(verificar);
+
+//03
+let idadeU = 25;
+let verificarA = idadeU >= 18 ? "O usuário é maior de idade!":"O usuário é menor de idade!"
+console.log(verificarA);
+
+//04
+let numero = 10;
+let verificarPI = numero % 2 != 0 ? "O número é ímpar!":"O número é par!"
+console.log(verificarPI)
