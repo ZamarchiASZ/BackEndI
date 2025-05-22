3b70d7ac2f7ba7116c07ce9efcdc176e612425e1
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-05-22 11:40:21 -0400

    fgfgfg

--- estruturasderepetilcao/atividadeDoWhile.js
+++ estruturasderepetilcao/atividadeDoWhile.js
@@ -1,7 +1,10 @@
 import rl from 'readline-sync';
 
-let num = rl.question("iNFORME UM NUMERO PARA OBTER A TABUADA: ");
+let num = rl.questionInt("iNFORME UM NUMERO PARA OBTER A TABUADA: ");
 
-while(num){
-    
+let cont = 1;
+
+while(contador <= 10)  {
+    console.log(${num})
+
 }
