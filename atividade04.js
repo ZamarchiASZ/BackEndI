830af15fa57d2ed3976aea589c9ef119d95a8217
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-05-22 10:56:52 -0400

    22/05/2025

--- atividade04.js
+++ atividade04.js
@@ -29,20 +29,99 @@ switch(dia) {
 }
 
 //2
-let mes = mesesdoAno.question('Digite um numero entre 1 á 12: ')
+let mes = Number(entradaDados.question('Digite um numero entre 1 á 12: '))
+//let mes = readLine.questionInt
 switch(mes) {
-    case "1":
+    case 1:
         console.log("Janeiro");
         break
-    case "2":
-        console.log("Janeiro");
+    case 2:
+        console.log("Fevereiro");
          break
-    case "1":
-        console.log("Janeiro");
+    case 3:
+        console.log("Março");
         break
-    case "1":
-        console.log("Janeiro");
+    case 4:
+        console.log("Abril");
         break
-        
+    case 5:
+        console.log("Maio");
+        break
+    case 6:
+        console.log("Junho");
+        break
+    case 7:
+        console.log("Julho");
+        break
+    case 8:
+        console.log("Agosto");
+        break
+    case 9:
+        console.log("Setembro");
+        break
+    case 10:
+        console.log("Outubro");
+        break
+    case 11:
+        console.log("Novembro");
+        break
+    case 12:
+        console.log("Dezembro");
+        break
 
+    default:
+        console.log('Besta não sabe que só vai até 12?');
+        break
+
+}
+
+let a = entradaDados.question("Informe o primeira numero: ")
+let operacao = entradaDados.question("Informe a operação que deseja: ")
+let b = entradaDados.question("Informe o segundo numero: ")
+
+let divisao = [a / b]
+let adicao = [a + b]
+let multiplicao = [a * b]
+let subtracao = [a - b]
+
+switch(a,b,operacao){
+    case a,b,operacao = "*":
+    console.log("O resultado será igual a:",multiplicao)
+    break
+
+    case a,b,operacao = "-":
+    console.log("O resultado será igual a:",subtracao)
+    break
+
+    case a,b,operacao = "+":
+    console.log("O resultado será igual a:",adicao)
+    break
+
+    case a,b,operacao = "/":
+    console.log("O resultado será igual a:",divisao)
+    break
+
+}
+
+//4
+let categoria = entradaDados.question('Digite sua categoria: A, B, C ou D\n ')
+let salario = entradaDados.questionFloat('Digite seu salário').toUpperCase();
+let bonus = 7
+
+switch(categoria){
+    case 'A':
+        console.log(`Seu salário atual vai ser de: ${salario - ((salario/100) * 5).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.`);
+        break
+    case 'B':
+        console.log(`Seu salário atual vai ser de: ${salario - ((salario/100) * 10).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.`);
+        break
+    case 'C':
+        console.log(`Seu salário atual vai ser de: ${salario - ((salario/100) * 15).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.`);
+        break
+    case 'D':
+        console.log(`Seu salário atual vai ser de:',3 ${salario - ((salario/100) * 20).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.`);
+        break
+    default:
+        console.log('Categoria inválida, digite uma letra de A a D, conforme sua categoria');
+        break
 }
