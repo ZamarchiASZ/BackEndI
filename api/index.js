2566de38bd5b28b238de641b057b49d88bfe72c5
ZamarchiASZ <zamarchi.a@estudante.ifro.edu.br>
2025-06-12 11:41:30 -0400

    node

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log("Serviço node inicializado em: " + data);
+})
